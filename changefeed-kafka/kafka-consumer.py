53cae9ce872aa2b6626d8765bb507a21de82dd6d
Julian Levi Hernandez <35246411+levihernandez@users.noreply.github.com>
2022-11-17 09:17:16 -0500

    Update kafka-consumer.py

--- changefeed-kafka/kafka-consumer.py
+++ changefeed-kafka/kafka-consumer.py
@@ -1,1 +1,14 @@
+from kafka import KafkaConsumer
+from json import loads
 
+consumer = KafkaConsumer(
+    'crl_users',
+     bootstrap_servers=['192.168.86.62:29092'],
+     auto_offset_reset='latest',
+     enable_auto_commit=True,
+     group_id=None,
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print("Topic Message:   ", message)
