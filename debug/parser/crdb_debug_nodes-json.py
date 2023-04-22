6a1247118a1ae029787044e020cae12df0f63b90
Julian Levi Hernandez <35246411+levihernandez@users.noreply.github.com>
2023-04-21 21:55:25 -0400

    Update crdb_debug_nodes-json.py

--- debug/parser/crdb_debug_nodes-json.py
+++ debug/parser/crdb_debug_nodes-json.py
@@ -7,6 +7,7 @@
 # 
 # Prerequisites:
 # * Create the Python environment and activate it
+# 
 # ```shell
 # python -m venv venv
 # source venv/bin/activate
@@ -19,11 +20,16 @@
 # ```
 # 
 # * Generate the debug bundle for the CockroachDB cluster and extract the zip file `nodes.json`
-# ```
-# cockroach debug zip debug.zip --certs-dir=certs --host=<node-ip>:26257
 # 
 # ```
+# cockroach debug zip debug.zip --certs-dir=certs --host=<node-ip>:26257
+# ```
+# 
 # * Validate the file location and Run the JupyterLab project
+# 
+# ```shell
+# jupyter-lab
+# ```
 
 # In[418]:
 
